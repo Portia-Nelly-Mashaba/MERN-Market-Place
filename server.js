6f2544a20cd0bbf7863e45a022a74f8d4f715a4c
Portia Nelly Mashaba <pnpnelly@gmail.com>
2024-11-05 01:39:13 +0200

    added database pull request

--- server.js
+++ server.js
@@ -6,15 +6,34 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+const productsModel = require('./models/productsModel');
+
 app.get('/', (req, res) => {
     res.send('this is from backend');
 });
 
 mongoose.connect('mongodb+srv://pnpnelly:PfdxkAYMl9G3flOn@backend.gdsq2.mongodb.net/?retryWrites=true&w=majority&appName=backend')
-app.listen(5000, () =>{
-    console.log('Server is Running');
+.then(() => {
+    console.log('Connected to database!');
+    app.listen(5000, () => {
+        console.log('Server is running on port 5000')
+    })
+})
+.catch(() =>{
+    console.log('Connection failed!');
     
 })
+
+//Products API
+app.get('/getproducts', (req, res) => {
+    productsModel.find({})
+        .then(docs => res.send(docs ))
+        .catch(err => res.status(400).json({ message: 'Something went wrong', error: err }));
+});
+// app.listen(5000, () =>{
+//     console.log('Server is Running');
+    
+// })
     
 
 
